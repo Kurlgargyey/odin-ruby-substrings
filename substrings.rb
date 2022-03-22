91c7ee3c752a372274b7c623a223d21c899e9d59
Kurlgargyey <nahsirkivar@gmail.com>
2022-03-22 17:48:29 +0100

    added user input and some deco

--- substrings.rb
+++ substrings.rb
@@ -1,17 +1,22 @@
 def substring_counter(string, dictionary)
-# 
-  tally = dictionary.reduce(Hash.new(0)) do |tally, word|
-# The #scan String method returns an Array with every match
+  tally = dictionary.reduce({}) do |tally, word|
+# The #scan String method returns an Array with every match already
     tally[word] = string.scan(/#{word}/i).length
+# Drop words with 0 occurences
+    tally.delete_if{|word,occ| occ == 0}
     tally
   end
-# Drop words with 0 occurences
-  tally.delete_if{|word,occ| occ == 0}
-  tally
 end
 
 dictionary = ["told", "you", "so", "obviously","yes","no"]
-string = "Told you so! Told Told Told you so! obviously obviously obviously obviously yes"
+#string = "Told you so! Told Told Told you so! obviously obviously obviously obviously yes o bvi o us ly toldyouso"
 #word ="told"
 #p string.scan(/#{word}/i)
-p substring_counter(string,dictionary)
+puts "Please enter a string to be searched:"
+string = gets.chomp
+puts "\n"
+puts "Searching the string for words in my dictionary... \n"
+puts "This is what I found:"
+substring_counter(string,dictionary).each do |word, occ|
+  puts "\"#{word}\" occurred #{occ} times."
+end
