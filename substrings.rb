4e9b7319cb6f2e911d0ce1d8eb323f6b7e912c50
Kurlgargyey <nahsirkivar@gmail.com>
2022-03-22 17:39:23 +0100

    removed unnecessary if statement

--- substrings.rb
+++ substrings.rb
@@ -1,17 +1,17 @@
 def substring_counter(string, dictionary)
+# 
   tally = dictionary.reduce(Hash.new(0)) do |tally, word|
-    matches = string.scan(/#{word}/i)
-    if matches
-      tally[word.to_sym] = matches.length
-    end
+# The #scan String method returns an Array with every match
+    tally[word] = string.scan(/#{word}/i).length
     tally
   end
+# Drop words with 0 occurences
   tally.delete_if{|word,occ| occ == 0}
   tally
 end
 
 dictionary = ["told", "you", "so", "obviously","yes","no"]
-string = "Told you so! Told Told Told you so!"
+string = "Told you so! Told Told Told you so! obviously obviously obviously obviously yes"
 #word ="told"
 #p string.scan(/#{word}/i)
 p substring_counter(string,dictionary)
